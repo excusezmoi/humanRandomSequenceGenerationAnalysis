e955fbd510222946ef2908b93ad99f5f7447eb98
Shiu-Cheng Wang <75075214+excusezmoi@users.noreply.github.com>
2023-06-22 17:59:09 +0800

    fixed

--- similarityCorrelation.py
+++ similarityCorrelation.py
@@ -7,15 +7,14 @@ import numpy as np
 from main import startToSimilarMatrix
 
 #Calculate the correlation of two similarity matrices
-def corOfSimilarityMatrices(similarityMatrix1, similarityMatrix2):
-    cor = np.corrcoef(similarityMatrix1.flatten(), similarityMatrix2.flatten())[0,1]
-    return cor
+def corOfMatrices(similarityMatrix1, similarityMatrix2):
+    return np.corrcoef(similarityMatrix1.flatten(), similarityMatrix2.flatten())[0,1]
 
 #similarity matrix correlation
-def corOfSimilarityMatricesExe(numOrAct,participantNumber1, participantNumber2, totalParticipant):
+def corOfSimilarityMatrices(numOrAct,participantNumber1, participantNumber2, totalParticipant):
     similar1 = startToSimilarMatrix(participantNumber1, numOrAct, totalParticipant)
     similar2 = startToSimilarMatrix(participantNumber2, numOrAct, totalParticipant)
-    print(corOfSimilarityMatrices(similar1, similar2))
+    return corOfMatrices(similar1, similar2)
 
 if __name__ == "__main__":
     
@@ -24,4 +23,4 @@ if __name__ == "__main__":
     totalParticipant = 10
     numOrAct = "n"
 
-    corOfSimilarityMatricesExe(numOrAct,participantNumber1, participantNumber2, totalParticipant)
+    print(corOfSimilarityMatrices(numOrAct,participantNumber1, participantNumber2, totalParticipant))
