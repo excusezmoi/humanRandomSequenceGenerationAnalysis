e7075641f349f13df377272e2a6d6ee919916460
Shiu-Cheng Wang <75075214+excusezmoi@users.noreply.github.com>
2023-06-24 17:56:00 +0800

    update Cfindex.py

--- CfIndex.py
+++ CfIndex.py
@@ -2,24 +2,24 @@ from collections import Counter
 
 from utils import configFilePath, readResponseTxtFile
 
-def elementPlacing(li):
-    dic = {}
-    for order, element in enumerate(li):
-        dic.setdefault(element, []).append(order)
-    return dic
-
-def intervalCalc2(dic):
-    interval = [i - j for alternative in dic for i in dic[alternative] for j in dic[alternative] if i > j]
-    return interval
-
-def countInstances(lis):
-    return dict(Counter(lis))
-
-def sumOccurences(countdict, le):
-    return sum(countdict.values()) / le
 
 def CfIndex(li):
-    return sumOccurences(countInstances(intervalCalc2(elementPlacing(li))),len(li))
+    def elementPlacing(li):
+        dic = {}
+        for order, element in enumerate(li):
+            dic.setdefault(element, []).append(order)
+        return dic
+
+    def intervalCalc(dic):
+        interval = [i - j for alternative in dic for i in dic[alternative] for j in dic[alternative] if i > j]
+        return interval
+
+    def countInstances(lis):
+        return Counter(lis)
+
+    def sumOccurences(countdict, le):
+        return sum(countdict.values()) / le
+    return sumOccurences(countInstances(intervalCalc(elementPlacing(li))),len(li))
 
 #read file and calculate CfIndex
 def CfIndexExe(participantNumber, numOrAct, slowOrFast, totalParticipant, fileFolder):
@@ -28,8 +28,8 @@ def CfIndexExe(participantNumber, numOrAct, slowOrFast, totalParticipant, fileFo
     fileName = f'/p{participantNumber} {slowOrFast}{"act" if numOrAct == "a" else "num" if numOrAct == "n" else None}.txt'
     filePath = fileFolder + fileName
 
-    response, length = readResponseTxtFile(filePath)
-    # print(response, length)
+    response, _length = readResponseTxtFile(filePath)
+
     return CfIndex(response)
     
 if __name__ == "__main__":
