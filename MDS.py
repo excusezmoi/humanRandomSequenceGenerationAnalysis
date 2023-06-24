237363b5cf13237f0af8c5215572f463b6098a8c
Shiu-Cheng Wang <75075214+excusezmoi@users.noreply.github.com>
2023-06-24 15:53:00 +0800

    OOP in MDS.py

--- MDS.py
+++ MDS.py
@@ -7,61 +7,63 @@ from sklearn.manifold import MDS
 
 from utils import startToSimilarMatrix, similarityToDissimilarity, startToReadCSVAndConvertToFloat, createMatrix, toSimilarityMatrix
 
-#Plot the MDS
-def plotMDS(dissimilar, numOrAct):
-    
-    mds = MDS(n_components = 2, dissimilarity = "precomputed", random_state=1)
-    pos = mds.fit_transform(dissimilar)
-    xs, ys = pos[:, 0], pos[:, 1]
+class MDSPlotter:
+    def __init__(self, numOrAct, totalParticipant):
+        self.numOrAct = numOrAct
+        self.totalParticipant = totalParticipant
+        self.df = startToReadCSVAndConvertToFloat(totalParticipant)
+        # self.similar = startToSimilarMatrix(participantNumber, numOrAct, totalParticipant)
+        # self.dissimilar = similarityToDissimilarity(self.similar)
 
-    labels = [f"{i}" for i in range(1,7)] if numOrAct == "n" else ["L","Q","S","W","R","J"] if numOrAct == "a" else None
+    def plotMDS(self, dissimilar):
+        mds = MDS(n_components = 2, dissimilarity = "precomputed", random_state=1)
+        pos = mds.fit_transform(dissimilar)
+        xs, ys = pos[:, 0], pos[:, 1]
 
-    for x, y, label in zip(xs, ys, labels):
-        plt.scatter(x, y, marker='o', color='purple')
-        plt.text(x, y, label, fontsize=15, color = 'darkblue')
-    plt.show()
+        labels = [f"{i}" for i in range(1,7)] if self.numOrAct == "n" else ["L","Q","S","W","R","J"] if self.numOrAct == "a" else None
 
-#Plot the MDS of a participant
-def plotPersonalMDS(participantNumber, numOrAct, totalParticipant):
-    similar = startToSimilarMatrix(participantNumber, numOrAct, totalParticipant)
-    dissimilar = similarityToDissimilarity(similar)
-    plotMDS(dissimilar, numOrAct)
+        for x, y, label in zip(xs, ys, labels):
+            plt.scatter(x, y, marker='o', color='purple')
+            plt.text(x, y, label, fontsize=15, color = 'darkblue')
+        plt.show()
 
-#Plot all the MDS
-def plotMDSonSubPlots(dissimilar, numOrAct, sub):
-    
-    mds = MDS(n_components = 2, dissimilarity = "precomputed", random_state = 1)
-    pos = mds.fit_transform(dissimilar)
-    xs, ys = pos[:, 0], pos[:, 1]
-
-    labels = [f"{i}" for i in range(1,7)] if numOrAct == "n" else ["L","Q","S","W","R","J"] if numOrAct == "a" else None
-    labelColors = {'L': 'red', 'Q': 'orange', 'S': 'yellow', 'W': 'green', 'R': 'blue', 'J': 'purple'} if numOrAct == "a" else {'1': 'red', '2': 'orange', '3': 'yellow', '4': 'green', '5': 'blue', '6': 'purple'} if numOrAct == "n" else None
-
-    for x, y, label in zip(xs, ys, labels):
-        sub.scatter(x, y, marker='o', color=labelColors[label])
-
-    # Plot all the MDS
-def plotAllMDS(numOrAct, totalParticipant):
-    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(8, 8))
-    for i in range(totalParticipant):
-        participantNumber = i + 1
-        df = startToReadCSVAndConvertToFloat(totalParticipant) #number of participants
-        upperTriangle = createMatrix(df, participantNumber, numOrAct) #df, participantNumber, numOrAct
-        similar = toSimilarityMatrix(upperTriangle)
+    def plotPersonalMDS(self, participantNumber):
+        similar = startToSimilarMatrix(participantNumber, self.numOrAct, self.totalParticipant)
         dissimilar = similarityToDissimilarity(similar)
-        row, col= i // 3, i % 3
-        plotMDSonSubPlots(dissimilar, numOrAct, axs[row, col])
-    labelColors = {'L': 'red', 'Q': 'orange', 'S': 'yellow', 'W': 'green', 'R': 'blue', 'J': 'purple'} if numOrAct == "a" else {'1': 'red', '2': 'orange', '3': 'yellow', '4': 'green', '5': 'blue', '6': 'purple'} if numOrAct == "n" else None
-    handles = [plt.Line2D([], [], color = color, marker = 'o', linestyle = '', label = label) for label, color in labelColors.items()]
-    plt.legend(handles=handles)
-    plt.show()
+        self.plotMDS(dissimilar)
+
+    def plotMDSonSubPlots(self, dissimilar, sub):
+        mds = MDS(n_components = 2, dissimilarity = "precomputed", random_state = 1)
+        pos = mds.fit_transform(dissimilar)
+        xs, ys = pos[:, 0], pos[:, 1]
+
+        labels = [f"{i}" for i in range(1,7)] if self.numOrAct == "n" else ["L","Q","S","W","R","J"] if self.numOrAct == "a" else None
+        labelColors = {'L': 'red', 'Q': 'orange', 'S': 'yellow', 'W': 'green', 'R': 'blue', 'J': 'purple'} if self.numOrAct == "a" else {'1': 'red', '2': 'orange', '3': 'yellow', '4': 'green', '5': 'blue', '6': 'purple'} if self.numOrAct == "n" else None
+
+        for x, y, label in zip(xs, ys, labels):
+            sub.scatter(x, y, marker='o', color=labelColors[label])
+
+    def plotAllMDS(self):
+        fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(8, 8))
+        for i in range(self.totalParticipant):
+            participantNumber = i + 1
+            df = startToReadCSVAndConvertToFloat(self.totalParticipant)
+            upperTriangle = createMatrix(df, participantNumber, self.numOrAct)
+            similar = toSimilarityMatrix(upperTriangle)
+            dissimilar = similarityToDissimilarity(similar)
+            row, col= i // 3, i % 3
+            self.plotMDSonSubPlots(dissimilar, axs[row, col])
+        labelColors = {'L': 'red', 'Q': 'orange', 'S': 'yellow', 'W': 'green', 'R': 'blue', 'J': 'purple'} if self.numOrAct == "a" else {'1': 'red', '2': 'orange', '3': 'yellow', '4': 'green', '5': 'blue', '6': 'purple'} if self.numOrAct == "n" else None
+        handles = [plt.Line2D([], [], color=color, marker='o', linestyle='None', label=label) for label, color in labelColors.items()]
+        plt.legend(handles=handles, loc='lower right')
+        plt.show()
 
 if __name__ == "__main__":
     
-    participantNumber = 7
-    numOrAct = "a"
-    slowOrFast = "s"
+    participantNumber = 5
+    numOrAct = "n"
     totalParticipant = 9
-    
-    plotPersonalMDS(participantNumber, numOrAct, totalParticipant)
-    plotAllMDS(numOrAct, totalParticipant)
+
+    mds = MDSPlotter(numOrAct, totalParticipant)
+    mds.plotPersonalMDS(participantNumber)
+    mds.plotAllMDS()
