69f3452ac7164c0a6f0c25d8609ac7e1809293b5
Shiu-Cheng Wang <75075214+excusezmoi@users.noreply.github.com>
2023-09-28 10:29:21 +0800

    objective distance added

--- subjectiveDistance.py
+++ subjectiveDistance.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from utils import configFilePath, startToReadCSVAndConvertToFloat, readResponseTxtFile, correctDict
 from utils import configFilePath, startToReadCSVAndConvertToFloat2, readResponseTxtFile, correctDict2
+from Markov import MarkovChainAll
+
 #calculate the average distance of true random sequence and the generated sequence 
 
 def randomSubjectiveDistance(df, goodDict):
@@ -41,7 +43,7 @@ def printSubjectiveDistanceExe(participantNumber, numOrAct, slowOrFast, totalPar
     filePath = fileFolder + fileName
 
     response, length = readResponseTxtFile(filePath)
-    print(response, length)
+    # print(response, length)
     df = startToReadCSVAndConvertToFloat2(totalParticipant, numOrAct)
     df, dictGood = correctDict2(df, participantNumber)
     randomDistance = randomSubjectiveDistance(df, dictGood)
@@ -49,12 +51,78 @@ def printSubjectiveDistanceExe(participantNumber, numOrAct, slowOrFast, totalPar
     print("the average subjective distance if the sequence is random:", randomDistance, 
           "\nthe average actual distance from their random sequence:", averageDistance)
 
+def subjectiveDistanceDict(participantNumber, totalParticipant, fileFolder):
+    #Read the txt file
+
+    distanceDict = {}
+    distanceDict["subjectNumber"] = participantNumber
+
+    for numOrAct in ["n", "a"]:
+        cond = f"{'Num' if numOrAct == 'n' else 'Act'}"
+        for slowOrFast in ["s", "f"]:
+            fileName = f'/p{participantNumber} {slowOrFast}{"act" if numOrAct == "a" else "num" if numOrAct == "n" else None}.txt'
+            filePath = fileFolder + fileName
+
+            response, length = readResponseTxtFile(filePath)
+            df = startToReadCSVAndConvertToFloat2(totalParticipant, numOrAct)
+            df, dictGood = correctDict2(df, participantNumber)
+            randomDistance = randomSubjectiveDistance(df, dictGood)
+            averageDistance = actualSequenceSubjectiveDistance(df, response, length, dictGood, numOrAct)
+
+            distanceDict[f"{slowOrFast}{cond}Distance"] = averageDistance
+
+        
+        distanceDict[f"expectedAverage{cond}Distance"] = randomDistance
+    
+      
+    return distanceDict
+
+def allDistanceDict(participantNumber, totalParticipant, fileFolder):
+    theAnswer = MarkovChainAll(totalParticipant, fileFolder)
+    allDistanceDict = subjectiveDistanceDict(participantNumber, totalParticipant, fileFolder)
+    allDistanceDict["sNumPrac"] = getattr(getattr(theAnswer, "p" + str(participantNumber)), "snum").averageObjectiveDistance
+    allDistanceDict["fNumPrac"] = getattr(getattr(theAnswer, "p" + str(participantNumber)), "fnum").averageObjectiveDistance
+    allDistanceDict["realDistance"] = (allDistanceDict["sNumPrac"] + allDistanceDict["fNumPrac"]) / 2
+    return allDistanceDict
+
+def dictToXLSX(dic, file):
+    # add the dictionary as an additional row to an existing xlsx file with keys being existing column names
+    # and values being the values of the new row without using pandas
+
+    # Load the existing Excel file
+    existing_file_path = file
+    df_existing = pd.read_excel(existing_file_path)
+    # print(df_existing,"\n\n\n")
+
+    # Your new dictionary
+    new_dict = dic
+
+    # Convert the dictionary to a DataFrame
+    df_new = pd.DataFrame([new_dict])
+
+    # Append the new DataFrame to the existing DataFrame
+    df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+    # print(df_combined)
+
+    # Save the combined DataFrame back to the Excel file
+    df_combined.to_excel(existing_file_path, index=False)
+
+
 if __name__ == "__main__":
 
-    participantNumber = 17
-    numOrAct = "n"
+    participantNumber = 1
+    numOrAct = "a"
     slowOrFast = "s"
     totalParticipant = 24
     fileFolder = configFilePath("FOLDER", "responseFileFolder")
+    outputFile = configFilePath("FILES", "distanceDataXLSXFile")
 
-    printSubjectiveDistanceExe(participantNumber, numOrAct, slowOrFast, totalParticipant, fileFolder)
+    # printSubjectiveDistanceExe(participantNumber, numOrAct, slowOrFast, totalParticipant, fileFolder)
+    # print(subjectiveDistanceDict(participantNumber, totalParticipant, fileFolder))
+
+    # Dici = allDistanceDict(participantNumber, totalParticipant, fileFolder)
+    # print(Dici)
+    for participantNumber in range(1, totalParticipant+1):
+        Dici = allDistanceDict(participantNumber, totalParticipant, fileFolder)
+        dictToXLSX(Dici, outputFile)
+    print("done")
